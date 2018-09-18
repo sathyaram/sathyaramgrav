96bb25893cb7e3c342c48aaa8f526c24ec9ffa9c
Sathya Ram <sathyatheram@gmail.com>
2018-09-18 18:24:09 +0100

    Update loading overlay

--- themes/sathyaram/src/js/custom/03.loading-overlay.js
+++ themes/sathyaram/src/js/custom/03.loading-overlay.js
@@ -9,28 +9,10 @@ $(document).ready(function() {
   var $logoOver = $('#loader img');
   var $logoHead = $('main > header .logo');
 
-  // Get the location of the header's logo
-  var newTop = $logoHead.css('top');
-  // Hide the header's logo
-  $logoHead.css('opacity', '0');
-  // Wait until the logo is done spinning
-  $logoOver.on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
-    // Fade out the rest of the overlay
-    $overlay.addClass('transparent');
-    // If we're at the very top of the page, animate the logo to the same
-    // position as the header's logo
-    if ($(window).scrollTop() === 0) {
-      // Move the logo to the same position as the header's logo
-      $logoOver.animate({ top: newTop }, SR.overlay.dur, 'swing', function() {
-        // Switch the logos
-        $logoHead.css('opacity', '1');
-        $overlay.fadeOut();
-      });
-    } else {
-      // Switch the logos
-      $logoHead.css('opacity', '1');
-      $overlay.fadeOut();
-    }
-  });
+  // Wait for the page to settle in
+  setTimeout(function() {
+    // Get rid of the overlay
+    $overlay.addClass('gone');
+  }, 2000);
 
 });
